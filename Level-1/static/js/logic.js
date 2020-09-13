87beb238797adc1c1d9b7995597272a1ceb2b658
Hannah Lee <hgl14@case.edu>
2020-09-13 14:23:26 -0400

    deleted notes to self in code

--- Level-1/static/js/logic.js
+++ Level-1/static/js/logic.js
@@ -7,7 +7,7 @@ var myMap = L.map("map", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
+    accessToken: "pk.eyJ1IjoiaGFubmFoMWxlZSIsImEiOiJja2VjMm05bGEwMnAxMnp1bnlrdHhtM2d1In0.4kf7QTPnAAtPJ7P3gF11Wg"
   }).addTo(myMap);
 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
